363085257223db0a85dbba3699335962744cb970
Jaime-Reynoso-Berrum <jaime.reynoso@encora.com>
2025-07-16 01:57:12 -0600

    added filterTodos() method to todoService class

--- src/main/java/com/encoraSpark/todoWebApp/model/Todo.java
+++ src/main/java/com/encoraSpark/todoWebApp/model/Todo.java
@@ -11,13 +11,13 @@ public class Todo {
     @Size (max=120, message = "To do item can not pass 120 characters")
     private String todoItem;
 
-    private String priority;
+    private int priority;
     private LocalDate creationDate;
     private LocalDate dueDate;
     private LocalDate doneDate;
     private boolean completed;
 
-    public Todo(String todoItem, String priority, LocalDate dueDate) {
+    public Todo(String todoItem, int priority, LocalDate dueDate) {
         this.id = UUID.randomUUID();
         this.todoItem = todoItem;
         this.priority = priority;
@@ -30,7 +30,7 @@ public class Todo {
     //Getters
     public UUID getId() { return id;}
     public String getTodoItem() { return todoItem;}
-    public String getPriority() { return priority;}
+    public int getPriority() { return priority;}
     public LocalDate getCreationDate() { return creationDate;}
     public LocalDate getDueDate() { return dueDate;}
     public LocalDate getDoneDate() { return doneDate;}
@@ -39,7 +39,7 @@ public class Todo {
     //Setters
     public void setId(UUID id) { this.id = id; }
     public void setTodoItem(String todoItem) { this.todoItem = todoItem;}
-    public void setPriority(String priority) { this.priority = priority;}
+    public void setPriority(int priority) { this.priority = priority;}
     public void setCreationDate(LocalDate creationDate) { this.creationDate = creationDate;}
     public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate;}
     public void setDoneDate(LocalDate doneDate) { this.doneDate = doneDate;}
--- src/main/java/com/encoraSpark/todoWebApp/service/todoService.java
+++ src/main/java/com/encoraSpark/todoWebApp/service/todoService.java
@@ -4,34 +4,74 @@ import com.encoraSpark.todoWebApp.model.Todo;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class todoService {
     private final Map<UUID, Todo> todoItems = new HashMap<>();
 
-    //updates page with the current todo item
+    //updates page with the current todo items
     public void updateList(){
         //code needed here
     }
 
+    //filters todo items based on query search, priority, and completed status
+    public List<Todo> filterTodos(String query, int priority, Boolean completed){
+        List<Todo> finalList = new ArrayList<>();
+
+        // adds todoItem to finalList based on filter options
+        for(Todo todoItem : todoItems.values()) {
+
+            //filters results by query search
+            if(query != null && !query.isEmpty()) {
+                String lowerCaseQuery = query.toLowerCase();
+                String lowerCaseTodoItem = todoItem.getTodoItem().toLowerCase();
+
+                if(lowerCaseTodoItem.contains(lowerCaseQuery)) {
+                    continue;
+                }
+            }
+
+            // filters results by priority
+            if (todoItem.getPriority() != 4 && todoItem.getPriority() != priority) {
+                continue;
+            }
+
+            // filters results by completed, not completed, or all
+            if (completed != null && todoItem.isCompleted() != completed) {
+                continue;
+            }
+        }
+
+        return finalList;
+    }
+
     //adds a new todo item to the HashMap/List
-    public Todo addTodo(String todoItem, String priority, LocalDate dueDate){
+    public Todo addTodo(String todoItem, int priority, LocalDate dueDate){
         Todo newItem = new Todo(todoItem, priority, dueDate);
         todoItems.put(newItem.getId(), newItem);
+
         updateList();
         return newItem;
     }
 
     //edits the current Todo object
-    public Todo editTodo(UUID id, String todoItem, String priority, LocalDate dueDate){
+    public Todo editTodo(UUID id, String todoItem, int priority, LocalDate dueDate, Boolean delete){
         Todo currentItem = todoItems.get(id);
 
+        // deletes the current item from todoItems hashmap and the list
+        if(delete){
+            todoItems.remove(id);
+            updateList();
+            return null;
+        }
+
         if(todoItem != null){ currentItem.setTodoItem(todoItem);}
-        if(priority != null){ currentItem.setPriority(priority);}
+        if(priority != currentItem.getPriority()){ currentItem.setPriority(priority);}
         if(dueDate != null){ currentItem.setDueDate(dueDate);}
+
+        updateList();
+        return currentItem;
     }
 
     //marks the current todo item as completed/done
@@ -39,6 +79,8 @@ public class todoService {
         Todo currentItem = todoItems.get(id);
         currentItem.setCompleted(true);
         currentItem.setDoneDate(LocalDate.now());
+
+        updateList();
         return currentItem;
     }
 
@@ -47,6 +89,8 @@ public class todoService {
         Todo currentItem = todoItems.get(id);
         currentItem.setCompleted(false);
         currentItem.setDoneDate(null);
+
+        updateList();
         return currentItem;
     }
 }
